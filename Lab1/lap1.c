5f2a555e22c64a360b6259155a715b09ee3eb4af
HaPhuong <haphuong2007gl@gmail.com>
2025-11-06 15:48:52 +0700

    Nop bai Lap1

---
+++ Lab1/lap1.c
@@ -0,0 +1,19 @@
+#Báo cáo cài đặt công cụ Visual Sudio Code:
++ Truy cập vào trang web chính thức của Visual Sudio Code.
++ Nhấp vào nút “Download for Windown”.
++ Chọn vị trí lưu tệp cài đặt trên máy tính.
++ Sau khi tải xuống hoàn tất, chạy tệp cài đặt vừa tải.
++ Chấp nhận các điều khoản và nhấp vào Tiếp (Next).
++ Nhấp vào “Install” để bắt đầu cài đặt.
++ Sau khi cài đặt hoàn tất có thể truy cập Visual Studio Code.
+#Kết nối với GitHub:
++ Tải và cài đặt Git từ trang chính thức.
++ Truy cập GitHub và mở Repository.
++ Nhấp vào nút <> Code trong phần HTTPS, sao chép đường dẫn URL của Repositoryđể lấy URL.
++ Mở Visual Studio Code.
++ Trên thanh Menu chính ở trên cùng, chọn View, chọn Command Palette.
++ Trong thanh tìm kiếm của Command Palette gõ “Git Clone”.
++ Dán đường dẫn URL đã sao chép vào thanh lệnh và nhấn Enter.
++ Một cửa sổ duyệt thư mục sẽ hiện ra.
++ Chọn một thư mục trống trên máy tính nơi muốn lưu trữ dự án.
++ Nhấn nút Select as Repository Destination sau đó nhấp vào open.
