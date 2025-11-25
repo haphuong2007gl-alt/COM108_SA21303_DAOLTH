2c715fd0ed7d77e090d398a1de0012260a55ffc2
Hà Phương <haphuong2007gl@gmail.com>
2025-11-25 21:55:32 +0700

    Nộp lab 2

---
+++ Lab2/Lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Nhap x: ");
+    scanf("%d", &x);
+    printf("Nhap y: ");
+    scanf("%d", &y);
+
+    printf("Tong = %d\n", x + y);
+    printf("Hieu = %d\n", x - y);
+
+    return 0;
+}
