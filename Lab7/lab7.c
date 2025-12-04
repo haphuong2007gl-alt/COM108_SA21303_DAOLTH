d935787c21702f3eaddc9d82c8acef040a39ce74
Hà Phương <haphuong2007gl@gmail.com>
2025-12-04 23:31:44 +0700

    Nộp Lab 7

--- Lab7/lab7.c
+++ Lab7/lab7.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
-int a[5][5];
-int i,j,teamp;
-int tong=0;
+#include <string.h>
+#include <ctype.h>
+
 int main() {
-    for(int i=0; i<5; i++)
-        for(int j=0;j<5;j++){
-        printf("Nhap cac phan tu a[%d]: ",i,j);
-        scanf("%d ",&a[i][j]);
+
+    // BÀI 1:  XÂY DỰNG CHƯƠNG TRÌNH ĐẾM NGUYÊN ÂM VÀ PHỤ ÂM CỦA 1
+    char str[200];
+    int vowel = 0, consonant = 0;
+
+    printf("Nhap chuoi: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = tolower(str[i]);
+
+        if (c >= 'a' && c <= 'z') {  // chỉ tính chữ cái
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                vowel++;
+            else
+                consonant++;
         }
-    printf("Xuat mang a[%d][%d]:\n",i,j);
-    for (int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++){
-        printf("%d ",a[i][j]);
-    }
-    printf("\n");
     }
 
-    for(int i=0;i<5;i++)
-        for (int j=0;j<5;j++)
-            tong+=a[i][j];
-    printf("\nTong mang = %d\n", tong);
+    printf("So nguyen am: %d\n", vowel);
+    printf("So phu am: %d\n", consonant);
+
+
+    //BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD
+    char user[50], pass[50];
+    char correctUser[] = "admin";
+    char correctPass[] = "123";
+
+    printf("\nNhap username: ");
+    scanf("%s", user);
+
+    printf("Nhap password: ");
+    scanf("%s", pass);
+
+    if (strcmp(user, correctUser) == 0 && strcmp(pass, correctPass) == 0) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap that bai!\n");
+    }
+
     return 0;
 }
